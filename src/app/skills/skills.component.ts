73c11122c8851ea3ed8d4748f9c13c684226d2c5
Sanjaya Shrestha <102684222+pasa2002@users.noreply.github.com>
2023-09-20 14:26:12 +0200

    skills added

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  skill=[
+    {
+      'name': 'Angular',
+      'image': '../../assets/icons/angular.svg',
+    },
+    {
+      'name': 'TypeScript',
+      'image': '../../assets/icons/ts.svg',
+    },
+    {
+      'name': 'JavaScript',
+      'image': '../../assets/icons/js.svg',
+    },
+    {
+      'name': 'HTML',
+      'image': '../../assets/icons/html.svg',
+    },
+    {
+      'name': 'Scrum',
+      'image': '../../assets/icons/scrum.svg',
+    },
+    {
+      'name': 'Firebase',
+      'image': '../../assets/icons/firebase.svg',
+    },
+    {
+      'name': 'Git',
+      'image': '../../assets/icons/git.svg',
+    },
+    {
+      'name': 'Css',
+      'image': '../../assets/icons/css.svg',
+    },
+    {
+      'name': 'Rest-Api',
+      'image': '../../assets/icons/api.svg',
+    },
+    {
+      'name': 'Material-design',
+      'image': '../../assets/icons/material-design.svg',
+    },
+  ]
+
 }
